001ca6ea80f0117a0d375d1fb1cbe79a8707790d
Seyed Morteza Hosseini <mortezaho.1376@gmail.com>
2024-05-29 15:19:16 -0600

    Support Rotary positional embedding for arm with float16

--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -777,6 +777,10 @@ void Transformations::PostLpt() {
     // Execute before snippets. Otherwise FQ will be converted to Subgraph
     CPU_REGISTER_PASS_X64(postLPTPassManager, ConvertFqRnnToQuantizedRnn);
 
+<<<<<<< HEAD
+=======
+    CPU_REGISTER_PASS_X64(postLPTPassManager, ov::pass::EliminateStridedSlice);
+>>>>>>> 4d7e4059a1 (Support Rotary positional embedding for arm with float16)
     CPU_REGISTER_PASS_COMMON(postLPTPassManager, ov::pass::RoPEFusion);
     CPU_REGISTER_PASS_X64(postLPTPassManager, CausalMaskPreprocessFusion);
 
